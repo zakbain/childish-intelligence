e3461d52c7b37728468ec42e0e1eaae79ba8cea0
zakbain <zakbain@gmail.com>
2017-08-04 11:31:41 -0400

    comment cleanup

--- src/experiences/Experience.java
+++ src/experiences/Experience.java
@@ -1,7 +1,9 @@
 package experiences;
 
 /**
- * Anything an agent can experience. This is deliberately left open ended as the goal of the AI is to be able to experience as much as possible.
+ * Anything an agent can experience. This is deliberately left open ended as the
+ * goal of the AI is to be able to experience as much as possible.
+ * 
  * @author Zak_b
  *
  */
