766a77cbb089ef4c278d3220ac63d42d8d35ba95
zakbain <zakbain@gmail.com>
2017-08-04 15:00:26 -0400

    Laid out some initial logic for how the heart deals with emotions

--- src/emotions/Emotion.java
+++ src/emotions/Emotion.java
@@ -5,6 +5,9 @@ package emotions;
  * experience of consciousness, sensation and behavior that reflects the
  * personal significance of a thing, an event, or a state of affairs.
  * 
+ * These emotions act as ranges where an agent's heart will feel anywhere from
+ * none of that emotion to the maximum (where you can never hit the maximum)
+ * 
  * @author Zak_b
  *
  */
--- src/emotions/Heart.java
+++ src/emotions/Heart.java
@@ -22,7 +22,24 @@ public class Heart {
 
 	}
 
+	/**
+	 * Process the experience in a personal way. Very open ended to allow for a
+	 * wide range of reactions to experiences. One requirement is that as the
+	 * computer grows it learns new ways to process experiences.
+	 * 
+	 * @param experience
+	 */
 	public void processExperience(Experience experience) {
+
+	}
+
+	/**
+	 * The heart is responsible for normalizing the convex set to ensure that
+	 * the current state is within the bounds of the convex set. This function
+	 * is public because child classes may decide how they choose to normalize
+	 * the state.
+	 */
+	public void normalizeCurrentState() {
 
 	}
 }
